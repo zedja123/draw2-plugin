aaf8d016f0015b4599f6ebd234faa17237dfe59a
HichTala <98521878+HichTala@users.noreply.github.com>
2025-06-24 19:20:45 +0200

    clean up

--- src/plugin-main.cpp
+++ src/plugin-main.cpp
@@ -21,11 +21,12 @@ with this program. If not, see <https://www.gnu.org/licenses/>
 #include <obs-frontend-api.h>
 #include <QWidget>
 #include "DrawDock.hpp"
+
 extern "C" {
 #include "draw.h"
 }
-extern struct obs_source_info draw_filter;
-extern struct obs_source_info draw_source;
+
+
 
 OBS_DECLARE_MODULE()
 OBS_MODULE_USE_DEFAULT_LOCALE(PLUGIN_NAME, "en-US")
