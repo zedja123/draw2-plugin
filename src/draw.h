2db011490df7fdbf01118a8c1bbda53b1effb4bc
HichTala <98521878+HichTala@users.noreply.github.com>
2025-06-24 18:49:46 +0200

    include draw.h in main

--- src/draw.h
+++ src/draw.h
@@ -8,7 +8,10 @@
 #include <obs-module.h>
 #include <obs-source.h>
 #include <graphics/graphics.h>
-#include <graphics/effect.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 struct draw_filter_data {
 	obs_source_t *source;
@@ -17,20 +20,26 @@ typedef struct draw_filter_data draw_filter_data_t;
 
 struct draw_source_data {
 	obs_source_t *source;
-
 	uint32_t width;
 	uint32_t height;
 };
 typedef struct draw_source_data draw_source_data_t;
 
-static const char *draw_filter_get_name(void *type_data);
-static const char *draw_source_get_name(void *type_data);
-static void *draw_filter_create(obs_data_t *settings, obs_source_t *source);
-static void *draw_source_create(obs_data_t *settings, obs_source_t *source);
-static void draw_filter_destroy(void *data);
+extern struct obs_source_info draw_filter;
+extern struct obs_source_info draw_source;
+
+const char *draw_filter_get_name(void *type_data);
+const char *draw_source_get_name(void *type_data);
+void *draw_filter_create(obs_data_t *settings, obs_source_t *source);
+void *draw_source_create(obs_data_t *settings, obs_source_t *source);
+void draw_filter_destroy(void *data);
 uint32_t draw_source_get_height(void *data);
 uint32_t draw_source_get_width(void *data);
 void draw_filter_get_defaults(obs_data_t *settings);
 void draw_source_video_render(void *data, gs_effect_t *effect);
 
-#endif //DRAW_FILTER_HPP
+#ifdef __cplusplus
+}
+#endif
+
+#endif // DRAW_FILTER_HPP
