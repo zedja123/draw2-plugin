941d3f8cb057cf433df0bd95a17226b3f625d4e4
HichTala <98521878+HichTala@users.noreply.github.com>
2025-07-23 12:02:47 +0200

    Test cross-platform shared memory

--- src/draw.c
+++ src/draw.c
@@ -14,6 +14,14 @@
 
 #include <errno.h>
 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <unistd.h>
+
 const char *draw_source_get_name(void *type_data)
 {
 	UNUSED_PARAMETER(type_data);
